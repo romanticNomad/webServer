c79a62a370f31c0834812bb98453f277dd230193
romanticNomad <satvolume@gmail.com>
2025-12-18 07:38:14 +0000

    TcpListner to local host and port 7878

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listner = TcpListener::bind("127.0.0.1.7878").unwrap();
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+        println!("hola!!")
+    }
 }
