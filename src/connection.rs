01664e4d209c71cc52fa5f949f4fdf2edfe60081
romanticNomad <satvolume@gmail.com>
2025-12-18 10:26:22 +0000

    response

--- src/connection.rs
+++ src/connection.rs
@@ -1,12 +1,13 @@
-use std::{io::{BufRead, BufReader}, net::TcpStream};
+use std::{io::{BufRead, BufReader, Write}, net::TcpStream};
 
-pub fn recieve(stream: TcpStream) {
+pub fn io(mut stream: TcpStream) {
     let stream_data = BufReader::new(&stream);
-    let request: Vec<String> = stream_data
+    let _request: Vec<String> = stream_data
         .lines()
         .map(|data| data.unwrap())
         .take_while(|line| !line.is_empty()) //htpps request ends with \n\n.
         .collect();
 
-    println!("http request; {request:#?}")
+    let response = "HTTP/1.1 200 OK \r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listner.incoming() {
         let stream = stream.unwrap();
-        connection::recieve(stream);
+        connection::io(stream);
     };
 }
