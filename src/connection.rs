fb01dc6885f0abac9843b66a9407873d4aee86b0
romanticNomad <satvolume@gmail.com>
2025-12-18 16:05:55 +0000

    slow server simulation

--- src/connection.rs
+++ src/connection.rs
@@ -2,6 +2,7 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write}, 
     net::TcpStream,
+    //  thread, time::Duration,
 };
 
 pub fn io(mut stream: TcpStream) {
@@ -13,8 +14,11 @@ pub fn io(mut stream: TcpStream) {
         .unwrap();
 
     let (status_line, path) = if request == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "html/hello.html")
-    } else {
+            ("HTTP/1.1 200 OK", "html/hello.html") }
+    // } else if request == "GET /sleep HTTP/1.1" {
+    //     thread::sleep(Duration::from_secs(5));
+        //     ("HTTP/1.1 200 OK", "html/hello.html") } slow request block other requests on single thread server.
+    else {
         ("HTTP/1.1 404 NOT FOUND", "html/error.html")
     };
     let content = fs::read_to_string(path).unwrap();
