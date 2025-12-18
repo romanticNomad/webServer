61a832ba8840bfd00e48cd91d43b703d7afe9846
romanticNomad <satvolume@gmail.com>
2025-12-18 11:02:49 +0000

    error messege and refactoring

--- src/connection.rs
+++ src/connection.rs
@@ -14,12 +14,18 @@ pub fn io(mut stream: TcpStream) {
 
     if request == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
+        let contents = fs::read_to_string("html/hello.html").unwrap();
         let length = contents.len();
 
         let response = 
             format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let error_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("html/error.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{error_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
         stream.write_all(response.as_bytes()).unwrap();
     }
 }
